c1718d9b74e56a379ae300b0f1d229751541a296
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-25 20:07:41 -0700

    added about nested route with link .

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+const page = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>About Us</h1>
+    </main>
+  );
+};
+
+export default page;
--- app/amazing/approach/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <div>i using App Router for this app!</div>;
-};
-
-export default page;
