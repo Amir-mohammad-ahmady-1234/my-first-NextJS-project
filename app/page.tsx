3783cd4f94227cf6c75a3bd3305476f0be8904b3
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-26 11:24:50 -0700

    start meal project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Link from "next/link";
-import styles from "./page.module.css";
-import Header from "@/components/Header";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header />
-      <h1 className={styles.h1}>Hello NextJS!</h1>
-      <p>
-        <span>you can also see about page with that link : </span>
-        <Link style={{ color: "blue" }} href="/about">
-          About us
-        </Link>
-      </p>
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
--- components/Header.tsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <h2>in this page i want to fill my page with this words!</h2>;
-};
-
-export default Header;
