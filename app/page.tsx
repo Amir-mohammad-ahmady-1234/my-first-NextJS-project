18d28aa4fbb4285ce0d37b005b32318bd90fd571
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-26 10:47:03 -0700

    added custom component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 
+import Header from "@/components/header";
+
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <h1 className={styles.h1}>Hello NextJS!</h1>
       <p>
-        <Link style={{ color: "white" }} href="/about">
+        <span>you can also see about page with that link : </span>
+        <Link style={{ color: "blue" }} href="/about">
           About us
         </Link>
       </p>
---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h2>in this page i want to fill my page with this words!</h2>;
+};
+
+export default Header;
