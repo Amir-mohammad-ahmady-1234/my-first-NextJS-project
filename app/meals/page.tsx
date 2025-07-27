01dc9794ae2a9f537cc6dc73e5f8804410cb5000
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-27 12:54:40 -0700

    implement meal page.

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const MealsPage = () => {
   return (
-    <main>
-      <h1>meals page</h1>
-    </main>
+    <>
+      <header>
+        <h1 className={classes.header}>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Chose your favorite recipeand cook it yourself. it is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
