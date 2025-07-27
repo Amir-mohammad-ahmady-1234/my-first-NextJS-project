3bd5a002b815228bb40b86d3ab1a9d439bb5c4f2
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-26 18:31:01 -0700

    added more custom compo.

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -3,26 +3,30 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 const MainHeader = () => {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <Image src={logoImg} alt="a plate with food on it." priority />
-        NextLevel food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="a plate with food on it." priority />
+          NextLevel food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
