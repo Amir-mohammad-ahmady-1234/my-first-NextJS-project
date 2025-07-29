fc9aa9ff3c314f7e1c21f253940958d70059f79f
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-28 18:56:11 -0700

    adding server side input validation.

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,11 @@ import { MealType } from "@/types/mealType";
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
+function isInvalidText(text: string | File | null) {
+  if (typeof text !== "string") return;
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData: FormData) {
   const data = {
     title: formData.get("title"),
@@ -13,6 +18,21 @@ export async function shareMeal(formData: FormData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(data.title) ||
+    isInvalidText(data.summary) ||
+    isInvalidText(data.instructions) ||
+    isInvalidText(data.image) ||
+    isInvalidText(data.creator) ||
+    isInvalidText(data.creator_email) ||
+    (typeof data.creator_email === "string" &&
+      !data.creator_email.includes("@")) ||
+    !(data.image instanceof File) ||
+    data.image.size === 0
+  ) {
+    throw new Error("invalid input");
+  }
 
   await saveMeal(data as MealType);
   redirect("/meals");
